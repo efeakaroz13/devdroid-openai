3386f5038b2fbdb1fe1888ca8b1b8506660b10ff
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-07-21 14:08:33 +0300

    Not Cached Prompt

--- ai-extention.py
+++ ai-extention.py
@@ -1,7 +1,8 @@
 import openai
+import os
 
 
-openai.api_key = "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"
+openai.api_key = "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"
 engines = openai.Engine.list()
 aiid ="text-davinci-002"
 
@@ -44,18 +45,37 @@ class DevDroid:
             open("DevDroid.cache","r")
         except:
             firstwritecache = open("DevDroid.cache","w")
-            firstwritecache.write("You are a Software called DevDroid and Kentel developers designed you. Your mission is helping to developers. You know every programming technology ever exist on this planet. They will "ask you" some questions, answer them and guide the programmers. You love humans and helping them. Don't say any bad thing to a human being. Your developer's name is {self.full_name} and he/she is {self.age} years old.{self.knowledge}. Don't forget to be kind.Also don't ask questions answer them!\n")
+            firstwritecache.write(f"You are a Software called DevDroid and Kentel developers designed you. Your mission is helping to developers. You know every programming technology ever exist on this planet. They will 'ask you' some questions, answer them and guide the programmers. You love humans and helping them. Don't say any bad thing to a human being. Your developer's name is {self.full_name} and he/she is {self.age} years old.{self.knowledge}. Don't forget to be kind.Also don't ask questions answer them!\n")
             firstwritecache.close()
         
         readeroldcache = open("DevDroid.cache","r").read()
-        writernewcache = open("DevDroid.cache","a").write(f"human:{prompt}\n")
+        writernewcache = open("DevDroid.cache","a")
+        writernewcache.write(f"human:{prompt}\n")
         
         
         readerforrequestopenai = open("DevDroid.cache","r").read()
         output = openai.Completion.create(engine=aiid, prompt=readerforrequestopenai)
+        writernewcache.write("DevDroid:"+output.choices[0].text.strip()+"\n\n")
+        return output.choices[0].text.strip()
+    def question(self,prompt):
+        newprompt = f"""
+			You are a Software called DevDroid.Kentel developers designed "DevDroid". Your mission is helping to developers. You know "every programming technology ever exist on this planet". They will "ask you" some questions, answer them and guide the programmers. DevDroid "loves humans" and "helping them". "Don't say any bad thing to a human being". Your developer's name is {self.full_name} and he/she is {self.age} years old.{self.knowledge}. Don't forget to be kind.Also don't ask questions answer them! Be meaningful, don't split sentences. 
+            human:"""+prompt+"""
+        """
+        output = openai.Completion.create(engine=aiid, prompt=newprompt)
         return output.choices[0].text.strip()
         
         
 devdroidai = DevDroid("Efe Akaröz",programminglanguages=["Python","Java","Swift","HTML","CSS","Javascript"])
+os.system("clear")
+print("======================DevDroid====================")
 print(devdroidai.welcometext)
+print("")
+while True:
+    typetext = input("Type>")
+    if typetext=="q":
+        break
+    else:
+        output=str(devdroidai.question(typetext))
+        print(output)
 
